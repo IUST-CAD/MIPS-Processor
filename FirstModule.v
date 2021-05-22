73b70b08117b5b43705c8f1e797b748dbf3ef648
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-05-23 01:06:04 +0430

    Add first module source

--- FirstModule.v
+++ FirstModule.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    23:52:44 05/22/2021 
+// Create Date:    01:03:57 05/23/2021 
 // Design Name: 
-// Module Name:    TestModule 
+// Module Name:    FirstModule 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,8 +18,23 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module TestModule(
-    );
+module FirstModule(
+	      input[1:0] a,
+	      input[1:0] b,
+         input cin,
+	      output[1:0] sum,
+	      output cout
+	     );
+ 
+	wire [1:0] p,g,c;
 
+	assign p=a^b;
+	assign g=a&b;
+	  
+	assign c[0]=cin;
+	assign c[1]= g[0]|(p[0]&c[0]);
+	assign cout= g[1] | (p[1]&g[0]) | p[1]&p[0]&c[0];
+
+	assign sum=p^c;
 
 endmodule
