7f48828316e087b3e17fc3c43aacd52eb9cec953
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-05-22 23:54:34 +0430

    Add very first xilinx files

---
+++ TestModule.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:52:44 05/22/2021 
+// Design Name: 
+// Module Name:    TestModule 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module TestModule(
+    );
+
+
+endmodule
